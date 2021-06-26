a0b118c8ddbdec9828f2abcc96b0dad6ac51799c
kingnaldoo <reinaldosn2016@gmail.com>
2021-06-26 20:21:20 -0300

    Dia 5

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import '../styles/question.scss';
+import classes from 'classnames';
 
 type QuestionProps = {
     content: string;
@@ -8,11 +9,23 @@ type QuestionProps = {
         avatar: string;
     };
     children?: ReactNode;
+    isAnswered?: boolean;
+    isHighlighted?: boolean;
 }
 
-export function Question({content, author, children}: QuestionProps) {
+export function Question({
+    content, 
+    author, 
+    isAnswered = false,
+    isHighlighted = false,
+    children
+}: QuestionProps) {
     return(
-        <div className="question">
+        <div className={classes(
+            'question',
+            { answered: isAnswered },
+            { highlighted: isHighlighted && !isAnswered }
+        )}>
             <p>{content}</p>
             <footer>
                 <div className="user-name">
